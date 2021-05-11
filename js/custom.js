a4a8ff64e6d9ebbce7750d191636fbd686e39bf2
Adam Chapman <adam@steeplinedesign.com>
2021-05-11 09:00:37 -0600

    updates

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,10 @@
-import {events} from './events.js'
+// import {events} from './events.js'
 $(function () { 
     
     
     $("#bio-container").hide();
-    console.info(events);
-    $.each(events, function(i,v){
-        let eventDate = new Date(v.date);
-        let today = new Date();
-        let b = ``;
-        
-            b += `<li data-date="${v.date}">`;
-                    b += `<div class="card ${today < eventDate ? '' : 'compact'}">`;
-                        b += `<div class="card-header">`; 
-                            b += `<span>`;
-                                b += `<i class="fas fa-calendar-alt" title="date"></i>: <div>${v.date_long}</div>`;
-                            b += `</span>`;
-                            b += `<span>`;
-                                b += `<i class="fas fa-map-marker-alt" title="location"></i>: <div>${v.location}</div>`;
-                            b += `</span>`;
-                        b += `</div>`;
-                        b += `<div class="card-body">`;
-                            b += `<div class="label">HOST:</div>`;
-                            b += `<div>${v.host}</div>`;
-                            if (today < eventDate) {
-                                b += `<div class="label mx-top-1">DESCRIPTION:</div>`;
-                                b += `<div>${v.description ? v.description : 'Liberty Dynamic Instructor Course'}</div>`;
-                                b += `<div class="label mx-top-1">CONTACT:</div>`;
-                                b += `<div><a href="${v.register_link ? v.register_link : 'mailto:training@libertydynamic.com'}" target="_blank">${v.register_text ? v.register_text : 'training@libertydynamic.com'}</a></div>`;
-                            }
-                        b += `</div>`;
-                    b += `</div>`;
-                b += `</li>`;
-                console.info(b)
-        
-                if (today < eventDate) {
-                    $('#upcoming_events').append(b);
-                } else {
-                    $('#past_events').append(b);
-                }                  
-    });
+    // console.info(events);
     
-    //Move past events
-    // setTimeout(function(){
-    //     $.each($('#upcoming_events li'), function(i,v){
-    //         let eventDate = new Date($(v).attr('data-date'));
-            
-    //         let today = new Date();
-    //         // console.info(today)
-    //         // console.info(eventDate < today)
-    //         if (eventDate < today) {
-    //             $(v).appendTo('#past_events')
-    //             $(v).find('.noshow').remove();
-    //             $(v).find('.card').addClass('compact');
-    //         }
-    //     });
-    // },500)
     
 
     
@@ -67,7 +17,7 @@ $(function () {
                 <p>During his distinguished military and defense career, John has deployed to seven operational theaters in support of national strategic objectives.</p>
                 <p>Throughout 14 years of defense related service, John developed in depth knowledge regarding tactical weapons and an aptitude for developing technologies that enhance the effectiveness of tactical officers' missions. </p>
                 <p>Having lost friends and colleagues in the line of duty, John has an intimate knowledge of the impact inferior products can have in kinetic environments. John is intensely focused and driven to overcome the operational deficiencies of tools affecting safety. Providing the highest and best tactical advantage is always in his crosshairs.</p>
-                <a class="mx-1" href="https://www.linkedin.com/in/john-chapman-us/">
+                <a class="mx-1" href="https://www.linkedin.com/in/john-chapman-us/" target="_blank">
                 <i class="fab fa-linkedin fa-fw fa-2x"></i>
                 </a>`);
                 break;
@@ -75,6 +25,8 @@ $(function () {
                 return (`<h5>Jarrett Hill serves as a key Tactics and Training Subject Matter Expert</h5>
                 <p>With over twenty-years of tactical experience, Jarrett has served in the Marine infantry, as a respected federal agent, and in the private military sector.</p>
                 <p>An experienced and knowledgeable operator, Jarrett has advanced through numerous tactical entry schools. His eight years of rigorous service on tactical entry teams has provided him with the aptitude to provide cutting edge techniques and situational experience for Liberty’s product development.</p>
+                <a class="mx-1" href="https://www.linkedin.com/in/jarrett-hill-611580a/" target="_blank">
+                <i class="fab fa-linkedin fa-fw fa-2x"></i>
                 `)
                 break;
             case 'adam-bio':
@@ -83,7 +35,7 @@ $(function () {
                 <p>He helps strategize and execute Liberty Dynamic's digital and social media missions around the world.</p>
                 <p>Adam appreciates the great risk and sacrifice his brother and friends experienced in their operations and how the Liberty Dynamic flash-bang will greatly improve safety and tactical advantage.</p>
                 <p>Liberty Dynamic is disrupting an entrenched market in a relatively short period of time and he is proud of the intense interest and demand for the device from military, SWAT, DEA and other law enforcement teams around the world. </p>
-                <a class="mx-1" href="https://www.linkedin.com/in/adamachapman/">
+                <a class="mx-1" href="https://www.linkedin.com/in/adamachapman/" target="_blank">
                 <i class="fab fa-linkedin fa-fw fa-2x"></i>
                 </a>`);
                 break;
--- js/events.js
+++ js/events.js
@@ -1,133 +1,170 @@
-export let events = [
-    //2019 ================================>
-    {
-        date: '2019-05-20',
-        date_long: 'May 20, 2019',
-        location: 'Tampa, FL',
-        host: 'SOFIC Conference - US Military'
-    },
-    {
-        date: '2019-06-04',
-        date_long: 'June 04, 2019',
-        location: 'Sandusky, OH',
-        host: 'OTOA - Ohio Tactical Officers Association'
-    },
-    {
-        date: '2019-08-17',
-        date_long: 'August 17, 2019',
-        location: 'Orlando, FL',
-        host: 'NTOA - National Tactical Officers Association'
-    },
-    {
-        date: '2019-11-11',
-        date_long: 'November 11-15, 2019',
-        location: 'Orlando, FL',
-        host: 'SWAT Round-Up International'
-    },
-    //2020 ================================>
-    {
-        date: '2020-06-22',
-        date_long: 'June 22 - 26, 2020',
-        location: 'Eldorado, TX',
-        host: 'First Spear Movie Shoot',
-    },
-    {
-        date: '2020-11-10',
-        date_long: 'November 10, 2020',
-        location: 'Little Rock, AR',
-        host: 'DARC - Direct Action Resource Center ',
-    },
-    //2021 ================================>
-    {
-        date: '2021-03-14',
-        date_long: 'March 14-18, 2021',
-        location: 'Columbus, GA',
-        host: 'GTOA/ATOA Combined - Georgia & Alabama Tactical Officers Association',
-    },
-    {
-        date: '2021-03-30',
-        date_long: 'March 30, 2021',
-        location: 'Ft. Benning, GA',
-        host: '75th Ranger Regiment - TREDS Demo',
-        description: 'TREDS Live Demo - Alpha Prototype'
-    },
-    {
-        date: '2021-04-01',
-        date_long: 'April 1, 2021',
-        location: 'Camp Lejeune, NC',
-        host: 'MARSOC - TREDS Demo',
-        description: 'TREDS Live Demo - Alpha Prototype'
-    },
-    {
-        date: '2021-04-07',
-        date_long: 'April 7, 2021',
-        location: 'Dallas, TX',
-        host: 'TTPOA - Texas Tactical Police Officers Association',
-    },
-    {
-        date: '2021-04-23',
-        date_long: 'April 23, 2021',
-        location: 'Alliance OH',
-        host: 'Alliance Police Department',
-    },
-    {
-        date: '2021-04-28',
-        date_long: 'April 28, 2021',
-        location: 'Little Rock, AR',
-        host: 'Direct Action Resource Center'
-    },
-    {
-        date: '2021-04-27',
-        date_long: 'April 27-30, 2021',
-        location: 'Indianapolis, IN',
-        host: "ISOA - INDIANA SWAT OFFICER'S ASSOCIATION CONFERENCE",
-        register_link: "https://www.indianaswatofficersassociation.org/2021isoaconference",
-        register_text: 'ISOA Website'
-    },
-    {
-        date: '2021-06-07',
-        date_long: 'June 7-10, 2021',
-        location: 'Sandusky, OH',
-        host: "OTOA - OHIO TACTICAL OFFICER ASSOCIATION CONFERENCE",
-        register_link: "https://www.otoa.org/",
-        register_text: 'OTOA Website'
-    },
-    {
-        date: '2021-06-30',
-        date_long: 'June 30 - July 2, 2021',
-        location: 'Durham, NC',
-        host: "NCTOA - NORTH CAROLINA TACTICAL OFFICER ASSOCIATION CONFERENCE",
-        register_link: "https://www.ncswat.org/events",
-        register_text: 'NCTOA Website'
-    },
-    {
-        date: '2021-07-21',
-        date_long: 'July 21 - 22, 2021',
-        location: 'Nashville, TN',
-        host: "TACOPS",
-        register_link: "https://www.cvent.com/events/2021-tacops-south-tactical-training-conference-expo/registration-92fdc27c385c4811a1fa9d3ba9ecaeb9.aspx?fqp=true",
-        register_text: 'TACOPS Website'
-    },
-    {
-        date: '2021-08-09',
-        date_long: 'August 9, 2021',
-        location: 'Joliet, IL',
-        host: "Will County Sheriff Office",
-    },
-    {
-        date: '2021-08-16',
-        date_long: 'August 16, 2021',
-        location: 'Louisvill, KY',
-        host: "KTOA - Kentucky Tactical Officers Association",
-        register_link: "http://www.kentuckytacticalofficersassociation.org/2021-conference--trade-show.html",
-        register_text: 'KTOA Website'
-    },
-    {
-        date: '2021-08-20',
-        date_long: 'August 20, 2021',
-        location: 'Verona, NY',
-        host: "TACOPS",
-        register_link: "https://www.cvent.com/events/2021-nytoa-s-w-a-t-tactical-training-conference-expo/registration-30b0abd418764bdf8cba3a479fb997e3.aspx?fqp=true",
-        register_text: 'TACOPS Website'
-    },
-]
+$(function () { 
+    let events = [
+        //2019 ================================>
+        {
+            date: '2019-05-20',
+            date_long: 'May 20, 2019',
+            location: 'Tampa, FL',
+            host: 'SOFIC Conference - US Military'
+        },
+        {
+            date: '2019-06-04',
+            date_long: 'June 04, 2019',
+            location: 'Sandusky, OH',
+            host: 'OTOA - Ohio Tactical Officers Association'
+        },
+        {
+            date: '2019-08-17',
+            date_long: 'August 17, 2019',
+            location: 'Orlando, FL',
+            host: 'NTOA - National Tactical Officers Association'
+        },
+        {
+            date: '2019-11-11',
+            date_long: 'November 11-15, 2019',
+            location: 'Orlando, FL',
+            host: 'SWAT Round-Up International'
+        },
+        //2020 ================================>
+        {
+            date: '2020-06-22',
+            date_long: 'June 22 - 26, 2020',
+            location: 'Eldorado, TX',
+            host: 'First Spear Movie Shoot',
+        },
+        {
+            date: '2020-11-10',
+            date_long: 'November 10, 2020',
+            location: 'Little Rock, AR',
+            host: 'DARC - Direct Action Resource Center ',
+        },
+        //2021 ================================>
+        {
+            date: '2021-03-14',
+            date_long: 'March 14-18, 2021',
+            location: 'Columbus, GA',
+            host: 'GTOA/ATOA Combined - Georgia & Alabama Tactical Officers Association',
+        },
+        {
+            date: '2021-03-30',
+            date_long: 'March 30, 2021',
+            location: 'Ft. Benning, GA',
+            host: '75th Ranger Regiment - TREDS Demo',
+            description: 'TREDS Live Demo - Alpha Prototype'
+        },
+        {
+            date: '2021-04-01',
+            date_long: 'April 1, 2021',
+            location: 'Camp Lejeune, NC',
+            host: 'MARSOC - TREDS Demo',
+            description: 'TREDS Live Demo - Alpha Prototype'
+        },
+        {
+            date: '2021-04-07',
+            date_long: 'April 7, 2021',
+            location: 'Dallas, TX',
+            host: 'TTPOA - Texas Tactical Police Officers Association',
+        },
+        {
+            date: '2021-04-23',
+            date_long: 'April 23, 2021',
+            location: 'Alliance OH',
+            host: 'Alliance Police Department',
+        },
+        {
+            date: '2021-04-28',
+            date_long: 'April 28, 2021',
+            location: 'Little Rock, AR',
+            host: 'Direct Action Resource Center'
+        },
+        {
+            date: '2021-04-27',
+            date_long: 'April 27-30, 2021',
+            location: 'Indianapolis, IN',
+            host: "ISOA - INDIANA SWAT OFFICER'S ASSOCIATION CONFERENCE",
+            register_link: "https://www.indianaswatofficersassociation.org/2021isoaconference",
+            register_text: 'ISOA Website'
+        },
+        {
+            date: '2021-06-07',
+            date_long: 'June 7-10, 2021',
+            location: 'Sandusky, OH',
+            host: "OTOA - OHIO TACTICAL OFFICER ASSOCIATION CONFERENCE",
+            register_link: "https://www.otoa.org/",
+            register_text: 'OTOA Website'
+        },
+        {
+            date: '2021-06-30',
+            date_long: 'June 30 - July 2, 2021',
+            location: 'Durham, NC',
+            host: "NCTOA - NORTH CAROLINA TACTICAL OFFICER ASSOCIATION CONFERENCE",
+            register_link: "https://www.ncswat.org/events",
+            register_text: 'NCTOA Website'
+        },
+        {
+            date: '2021-07-21',
+            date_long: 'July 21 - 22, 2021',
+            location: 'Nashville, TN',
+            host: "TACOPS",
+            register_link: "https://www.cvent.com/events/2021-tacops-south-tactical-training-conference-expo/registration-92fdc27c385c4811a1fa9d3ba9ecaeb9.aspx?fqp=true",
+            register_text: 'TACOPS Website'
+        },
+        {
+            date: '2021-08-09',
+            date_long: 'August 9, 2021',
+            location: 'Joliet, IL',
+            host: "Will County Sheriff Office",
+        },
+        {
+            date: '2021-08-16',
+            date_long: 'August 16, 2021',
+            location: 'Louisvill, KY',
+            host: "KTOA - Kentucky Tactical Officers Association",
+            register_link: "http://www.kentuckytacticalofficersassociation.org/2021-conference--trade-show.html",
+            register_text: 'KTOA Website'
+        },
+        {
+            date: '2021-08-20',
+            date_long: 'August 20, 2021',
+            location: 'Verona, NY',
+            host: "TACOPS",
+            register_link: "https://www.cvent.com/events/2021-nytoa-s-w-a-t-tactical-training-conference-expo/registration-30b0abd418764bdf8cba3a479fb997e3.aspx?fqp=true",
+            register_text: 'TACOPS Website'
+        },
+    ]
+    $.each(events, function(i,v){
+        let eventDate = new Date(v.date);
+        let today = new Date();
+        let b = ``;
+        
+            b += `<li data-date="${v.date}">`;
+                    b += `<div class="card ${today < eventDate ? '' : 'compact'}">`;
+                        b += `<div class="card-header">`; 
+                            b += `<span>`;
+                                b += `<i class="fas fa-calendar-alt" title="date"></i>: <div>${v.date_long}</div>`;
+                            b += `</span>`;
+                            b += `<span>`;
+                                b += `<i class="fas fa-map-marker-alt" title="location"></i>: <div>${v.location}</div>`;
+                            b += `</span>`;
+                        b += `</div>`;
+                        b += `<div class="card-body">`;
+                            b += `<div class="label">HOST:</div>`;
+                            b += `<div>${v.host}</div>`;
+                            if (today < eventDate) {
+                                b += `<div class="label mx-top-1">DESCRIPTION:</div>`;
+                                b += `<div>${v.description ? v.description : 'Liberty Dynamic Instructor Course'}</div>`;
+                                b += `<div class="label mx-top-1">CONTACT:</div>`;
+                                b += `<div><a href="${v.register_link ? v.register_link : 'mailto:training@libertydynamic.com'}" target="_blank">${v.register_text ? v.register_text : 'training@libertydynamic.com'}</a></div>`;
+                            }
+                        b += `</div>`;
+                    b += `</div>`;
+                b += `</li>`;
+                
+        
+                if (today < eventDate) {
+                    $('#upcoming_events').append(b);
+                } else {
+                    $('#past_events').append(b);
+                }                  
+    });
+})
